43dbfff8fb8148c034e3102c402d77d4015c4bcf
Naem Haq <nhaq@ucc.ie>
2026-03-02 14:59:21 +0000

    feat(src): export batch NICE label helper

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,6 @@
 CTG reusable modules (preprocessing, segmentation, feature extraction, utils).
 """
 
-from .nice_labels import assign_3class_label
+from .nice_labels import assign_3class_label, assign_3class_labels
 
-__all__ = ["assign_3class_label"]
+__all__ = ["assign_3class_label", "assign_3class_labels"]
